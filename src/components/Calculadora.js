869a91c30a4ed3a5ebbacc65ff1c91beead96abb
@douglasabnovato <douglasabnovato@gmail.com>
2019-12-11 16:08:34 -0300

    details

--- src/components/Calculadora.js
+++ src/components/Calculadora.js
@@ -1,21 +1,39 @@
 import React from 'react';
 
 import Teclado from './Teclado';
+import Visor from './Visor';
 
+import "./App.css";
+
+import { calculadoraStore } from '../flux/CalculadoraStore';
 
 export default class Calculadora extends React.Component{
     
+    constructor(props){
+        super(props)
+        this.state = calculadoraStore.getState()
+    }
+    
+    componentWillMount(){
+        calculadoraStore.on('change', () => {
+            this.setState( calculadoraStore.getState() )
+        })
+    }
+
     render(){
 
         return ( 
-            <div>
+            <div id="calculadora"> 
                 <p>
                     <h3 align="center">
                         Voitto Treinamentos - Calculadora em React.js<br />
                     </h3>
                 </p>
-                
-                <Teclado/>
+
+                <form name="calculator">
+                    <Visor type="textfield" name="ans" value={this.state.valorDoDisplay} />                                     
+                    <Teclado/>
+                </form>
 
                 <p>
                     <h4 align="center">
--- src/components/Teclado.js
+++ src/components/Teclado.js
@@ -1,49 +1,40 @@
 import React from 'react';
 
 import Botao from './Botao';
-import Visor from './Visor';
 
-import "./App.css";
 
 export default class Teclado extends React.Component{
     
     render(){
         
-        return(
-            
-            <div id="teclado">                
-
-                <form name="calculator">
-                    <Visor type="textfield" name="ans" value="0" />
-                    <br />
-                    <Botao type="reset" value="C"/>
-                    <Botao type="button" value="+/-"/>
-                    <Botao type="button" value="%"/>
-                    <Botao type="button" value="/"/>
-                    <br />
-                    <Botao type="button" value="1"/>
-                    <Botao type="button" value="2"/>
-                    <Botao type="button" value="3"/>
-                    <Botao type="button" value="+"/>
-                    <br />
-                    <Botao type="button" value="4" />
-                    <Botao type="button" value="5"/>
-                    <Botao type="button" value="6" />
-                    <Botao type="button" value="-"/>
-                    <br />
-                    <Botao type="button" value="7" />
-                    <Botao type="button" value="8" />
-                    <Botao type="button" value="9"/>
-                    <Botao type="button" value="*" />
-                    <br />
-                    <Botao type="button" value="0"/>
-                    <Botao type="button" value="."/>
-                    <Botao type="button" value="pi"/>
-                    <Botao type="button" value="="/>
-                </form>
-
+        return(                                      
+            <div>
+                <br />
+                <Botao type="reset" value="c"/>
+                <Botao type="button" value="+/-"/>
+                <Botao type="button" value="%"/>
+                <Botao type="button" value="/"/>
+                <br />
+                <Botao type="button" value="1"/>
+                <Botao type="button" value="2"/>
+                <Botao type="button" value="3"/>
+                <Botao type="button" value="+"/>
+                <br />
+                <Botao type="button" value="4" />
+                <Botao type="button" value="5"/>
+                <Botao type="button" value="6" />
+                <Botao type="button" value="-"/>
+                <br />
+                <Botao type="button" value="7" />
+                <Botao type="button" value="8" />
+                <Botao type="button" value="9"/>
+                <Botao type="button" value="*" />
+                <br />
+                <Botao type="button" value="0"/>
+                <Botao type="button" value="."/>
+                <Botao type="button" value="pi"/>
+                <Botao type="button" value="="/>
             </div>
-
         )
 
     }
